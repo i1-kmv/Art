5537dc0c02f57d1f743524139a514666855bf5a8
i1-kmv <i1-kmv@mail.ru>
2020-07-05 13:17:58 +0200

    Настроил фильтрацию табов в блоке портфолио

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,8 @@ import forms from './modules/forms';
 import mask from './modules/mask';
 import showMoreStyles from './modules/showMoreStyles';
 import calc from './modules/calc';
+import filter from './modules/filter';
+
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -19,5 +21,7 @@ window.addEventListener('DOMContentLoaded', () => {
     showMoreStyles('.button-styles', '#styles .row');
 
     calc('#size', '#material', '#options', '.promocode', '.calc-price');
+
+    filter();
 
 });
